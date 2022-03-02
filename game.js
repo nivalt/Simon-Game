181e9ac19da99eb6fa4200f85347ab7555606cf3
Niv Alterman <nivalt92@gmail.com>
2022-03-02 20:46:20 +0200

    create user seq and playsound

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-alert("hello");
+// check response 
+// alert("hello");
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickPattern = [];
+
+
+$("button").click(function() {
+    var userChosenColor = $(this).attr("id");
+        console.log(userChosenColor);
+    userClickPattern.push(userChosenColor);
+        console.log(userClickPattern);
+    playSound(userChosenColor);
+});
+
+function nextSequence(){
+    var randNum = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randNum];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+    console.log(gamePattern);
+}
+
+function playSound(name){
+    var colorSound = new Audio("sounds/" + name + ".mp3");
+    colorSound.play();
+}
